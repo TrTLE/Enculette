6312e9a24977a80029b09c2e4192b5a09bd70e27
TrTLE <chpaqou@gmail.com>
2017-01-09 11:11:56 +0100

    Test commit

--- enculette/src/main/java/org/bzs/enculette/App.java
+++ enculette/src/main/java/org/bzs/enculette/App.java
@@ -4,10 +4,10 @@ package org.bzs.enculette;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+    System.out.println("END");
+  }
 }
